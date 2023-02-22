f363d42af3dea928b594fd46a48f0d3c1fceebd1
Anigold <38135750+Anigold@users.noreply.github.com>
2023-02-22 11:28:04 -0500

    Add files via upload

---
+++ Puzzle.java
@@ -0,0 +1,319 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Time;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.NoSuchElementException;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Puzzle {
+
+    /**Game class that maintains state and logic of the SpellingBee game.
+     * 
+     * Players create their owns puzzles according to the following rules:
+	 * 
+	 * 		1. The puzzle must contain exactly 7 characters.
+	 * 		2. Only letters from the English alphabet may be used.
+	 * 		3. A letter may be only used once.
+	 * 
+	 * The player can then guess words based upon the available letters and 
+	 * according to the following rules:
+	 * 
+	 * 		1. All words must be at least 4 letters.
+	 * 		2. Letters can be repeated as many times as needed.
+	 * 		3. Only letters found within the game board are valid.
+	 * 
+	 * Upon entry of a valid word, the word will be scored based upon the
+	 * following rules:
+	 * 
+	 * 		1. A word using 4 letters is worth 1 point, all other words
+	 * 			are worth their length (e.g. "chant" is worth 5 points).
+	 * 		2. An additional 7 points are awarded if the player uses all
+	 * 			7 of the letters.
+	 * 
+	 * When the player is finished, they may press "Solve" and the remaining
+	 * words will be added to the word list.
+     * 
+     */
+
+    String letters;
+    String pattern;
+    boolean hasPangram = false;
+
+    LinkedHashMap <String, HashMap<String, Integer>> currentWordList = new LinkedHashMap <String, HashMap<String, Integer>>();
+	/*
+	 * wordList <word, wordInfo> = {
+	 * 		
+	 * 		"word": {
+	 * 			"score": 1,
+	 * 			"pangram": false, 
+	 * 			"found": false,		
+	 * 		},
+	 * };
+	 */
+    
+    public Puzzle(String puzzleString) throws InvalidPuzzleException {
+
+        try {
+            this.letters = _isValidPuzzle(puzzleString);
+            this.pattern = _generatePuzzlePattern(puzzleString);
+        } catch (InvalidPuzzleException ipe) {
+            throw new InvalidPuzzleException(ipe.getMessage());
+        }
+
+    };
+
+	/**Helper function to produce the regular expression for the puzzle
+	 * 
+	 * Assumes the supplied puzzle is valid. Inserts letter checks from
+	 * the puzzle into the regular expression. 
+	 * 
+	 * @param puzzleString The puzzle to model the regular expression from
+	 * @return String representation of the regular expression
+	 */
+	private String _generatePuzzlePattern(String puzzleString) {
+		return String.format("^(?=[%s]{4,})(?=.*%s.*)(?!.*[^%s])[a-zA-Z]*$", 
+			puzzleString.toLowerCase(), 
+			puzzleString.toLowerCase().charAt(0), 
+			puzzleString.toLowerCase()
+			);
+		
+	};
+
+    /**Helper function to determine validity of a puzzle string.
+	 * 
+	 * Uses regular expressions to determine whether the puzzle string
+	 * falls within the following rules:
+	 * 
+	 * 		1. The puzzle must be exactly 7 characters.
+	 * 		2. Contains only letters from the English alphabet.
+	 * 		3. All letters are unique, no letter repeats.
+	 * 
+	 * @param puzzleString The puzzle to be checked
+	 * @return puzzleString if valid, otherwise throws exception
+	 * @throws InvalidPuzzleException Throws message detailing why the check failed
+	 */
+	private String _isValidPuzzle(String puzzleString) throws InvalidPuzzleException {
+
+		String pattern = "^(?!.*(.).*\\1)[a-zA-Z]{7}$";
+
+		if ( !puzzleString.matches(pattern) ) {
+			
+			if (puzzleString.matches("^.*[^a-zA-Z].*$")) {
+                throw new InvalidPuzzleException("Input string contains non-letter symbols.");
+            } else if (puzzleString.matches("^.*(.).*\\1.*$")) {
+                throw new InvalidPuzzleException("Input string contains repeated letters.");
+            } else if (!puzzleString.matches("^.{7}$")) {
+                throw new InvalidPuzzleException("Input string does not contain exactly 7 characters.");
+           } else {
+                throw new InvalidPuzzleException("Input string does not match the expected pattern.");
+            }
+		}
+		return puzzleString;	 
+	};
+
+    /**Helper function to determine validity of a guess.
+	 * 
+	 * A guess must fall within the following rules:
+	 *
+	 * 	1. The word must be at least 4 letters long.
+	 * 	2. The word can only use the letters provided by the game board.
+	 * 	3. The word must use the center letter.
+	 * 	4. The word must be found in the dictionary.
+	 * 	5. The word cannot have already been found.
+	 * 
+	 * @param guessString Player's guess to be checked
+	 * @return
+	 * @throws InvalidGuessException
+	 */
+	public String isValidGuess(String guessString) throws InvalidGuessException {
+	
+		boolean inDict = this.currentWordList.containsKey(guessString);
+		boolean usesLetters = guessString.matches(String.format("^[%s]*$", this.letters.toLowerCase()));
+		boolean isSize = guessString.length() >= 4;
+		boolean usesCenter = guessString.matches(String.format("^(?=.*%s.*)[a-zA-Z]*$", this.letters.toLowerCase().charAt(0)));
+		boolean isFound = inDict && (this.currentWordList.get(guessString).get("found") != 1);
+		
+		if (!isSize) {
+			throw new InvalidGuessException("Not enough letters.");
+		} else if (!usesLetters) {
+			throw new InvalidGuessException("Word uses invalid letter.");
+		} else if (!usesCenter) {
+			throw new InvalidGuessException("Word does not use center letter.");
+		} else if (!inDict) {
+			throw new InvalidGuessException("Word was not found in dictionary.");
+		} else if (!isFound) {
+			throw new InvalidGuessException("Already found.");
+		}
+
+		return guessString;
+	}
+
+    /**Helper function to load word list from file.
+	 * 
+	 * Scans file for words that fit the pattern. Loads
+	 * matching words to the accepted words list.
+	 * 
+	 * @param path The string representation of the path to the file
+	 * @param pattern The Regular Expression pattern to be tested against
+	 * @throws FileNotFoundException 
+	 */
+	public void loadWordListFromFile(String path) throws FileNotFoundException {
+		this.currentWordList.clear();
+		
+		try {
+			Scanner fileScanner = new Scanner(new File(path));
+			
+			while ( fileScanner.hasNext() ) {
+				String word = fileScanner.next();
+				if (!word.matches(this.pattern)) {
+                    continue;
+				}	
+                
+                // This could be a good time to use Prototype Pattern
+                HashMap<String, Integer> wordInfo = new HashMap<String, Integer>();
+
+                int wordScore = _scoreWord(word);
+                int pangram = _isPangram(word) ? 1 : 0;
+
+                if (!hasPangram && pangram == 1) {
+                    hasPangram = true;
+                }
+                wordInfo.put("score", wordScore);
+                wordInfo.put("pangram", pangram);
+                wordInfo.put("found", 0);
+
+                this.currentWordList.put(word, wordInfo);
+			};
+
+			fileScanner.close();
+            
+		} catch (FileNotFoundException fnf) {
+			throw new FileNotFoundException(String.format("File not found at path: %s", path));
+		} catch (NoSuchElementException nse) {
+			throw new NoSuchElementException();
+		}
+	};
+
+    /**Functional but incomplete...
+	 * 
+	 * Wanted to generate an exhaustive list of puzzles that had between
+	 * 21 and 81 words as well as a pangram. Did the math, there will be
+	 * 26 choose 7 = 657,800 different combinations.
+	 * 
+	 * Wrote an inefficient nChoosek algorithm, after an hour of running
+	 * it handed out ~300 solutions. This randomizer works functionally, 
+	 * the rest of the list just needs to be generated.
+	 * 
+     * A consequence of this is that all random puzzles have a center letter with A.
+     * 
+     */
+    public static Puzzle generateRandomPuzzle() throws FileNotFoundException, InvalidPuzzleException {
+        
+        try {
+            Scanner fileScanner = new Scanner(new File("ExhaustiveList.txt"));
+            fileScanner.useDelimiter(Pattern.compile("(\\n)|;"));
+            Random rand = new Random();
+            rand.setSeed(System.currentTimeMillis());
+
+            String puzzle = "acegiop";
+            int upperbound = 300;
+
+            int randomInt = rand.nextInt(upperbound);
+            
+            int lineCount = 0;
+            while ( fileScanner.hasNext() ) {
+                if (lineCount == randomInt) {
+
+                    puzzle = fileScanner.next();
+                    break;
+                }
+                fileScanner.next();
+                ++lineCount;
+            }
+
+            return new Puzzle(puzzle.split(" ")[0]);
+
+        } catch (FileNotFoundException fnf) {
+            throw new FileNotFoundException(String.format("No file found at path: ./ExhaustiveList.txt"));
+        } catch (InvalidPuzzleException ip) {
+            throw new InvalidPuzzleException(ip.getMessage());
+        }
+       
+
+    }
+
+    /**Helper function to determine if a word is a pangram.
+	 * 
+	 * Checks the word to see if it uses 7 distinct letters.
+	 * It assumes the word is valid within the puzzle.
+	 * 
+	 * @param word The word to be checked
+	 * @return boolean
+	 */
+	private boolean _isPangram(String word) {
+		return word.chars().distinct().count() == 7;
+	};
+
+	/**Helper function to determine score of a given word.
+	 * 
+	 * Scored based on the following rules:
+	 * 		1. A word using 4 letters is worth 1 point, all other words
+	 * 			are worth their length (e.g. "chant" is worth 5 points).
+	 * 		2. An additional 7 points are awarded if the player uses all
+	 * 			7 of the letters (i.e. the word is a pangram).
+	 * 
+	 * @param word The word to be checked
+	 * @return int value of the word score
+	 */
+	private int _scoreWord(String word) {
+		int baseScore = word.length() == 4 ? 1 : word.length();
+		int pangramScore = (_isPangram(word)) ? 7 : 0; 
+
+		return baseScore + pangramScore;	
+	}
+	
+    /**Getter function to calculate current score.
+	 * 
+	 * Iterates through current accepted words list, tallying
+	 * the score when a word is flagged as found.
+	 * 
+	 * @return int value of the total score
+	 */
+	public int getCurrentScore() {
+		int totalScore = 0;
+
+		for (String word : this.currentWordList.keySet()) {
+			HashMap<String, Integer> wordInfo = this.currentWordList.get(word);
+			if (wordInfo.get("found") == 1) {
+				int wordScore = wordInfo.get("score");
+				totalScore += wordScore;
+			}
+		}
+		return totalScore;
+	}
+	
+	/**Getter function to tally the number of found words.
+	 * 
+	 * Iterates through current accepted words list, tallying
+	 * the count when a word is flagged as found.
+	 * 
+	 * @return int value of the total word count
+	 */
+	public int getCurrentWordCount() {
+		int totalWords = 0;
+
+		for (String word : this.currentWordList.keySet()) {
+			HashMap<String, Integer> wordInfo = this.currentWordList.get(word);
+			if (wordInfo.get("found") == 1) {
+				++totalWords;
+			}
+		}
+		return totalWords;
+		
+	}
+	
+}
+
---
+++ SpellingBee.java
@@ -0,0 +1,224 @@
+import java.awt.Color;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.HashMap;
+
+
+public class SpellingBee {
+	/**
+	 * Intermediary run-time class to couple SpellingBeeGraphics to Puzzle logic.
+	 * 
+	 * All methods are callback functions propagating from SpellingBeeGraphics
+	 * and invoking Puzzle class logic. All exceptions from the Puzzle class are
+	 * passed to the SpellingBeeGraphics message handler to be displayed.
+	 * 
+	 */
+
+	/**
+	 * Initializes the game board and field callbacks.
+	 */
+	public void run() {
+
+		sbg = new SpellingBeeGraphics();
+		sbg.addButton("Shuffle", (s) -> shuffleAction());
+		sbg.addField("Puzzle", (s) -> puzzleAction(s));
+		sbg.addButton("Random", (s) -> randomizeAction());
+		sbg.addField("Word", (s) -> guessAction(s));
+		sbg.addButton("Solve", (s) -> solveAction());
+
+	};
+
+	/**
+	 * Callback function for "Puzzle" input field.
+	 * 
+	 * Determines validity of puzzle input. If valid, generates
+	 * the game board and loads the accepted words list to memory.
+	 * 
+	 * @param puzzleString The puzzle letters to be verified
+	 */
+	private void puzzleAction(String puzzleString) {
+		sbg.showMessage(" ");
+		sbg.clearWordList();
+
+		try {
+
+			puzzle = new Puzzle(puzzleString);
+			sbg.setBeehiveLetters(puzzleString);
+			puzzle.loadWordListFromFile(ENGLISH_DICTIONARY);
+
+		} catch (InvalidPuzzleException e) {
+			sbg.showMessage(e.getMessage(), Color.RED);
+		} catch (FileNotFoundException fnf) {
+
+		}
+	}
+
+	/**
+	 * Callback function for "Solve" button.
+	 * 
+	 * Loads the word list to the game board and displays
+	 * scoring.
+	 */
+	private void solveAction() {
+
+		sbg.clearWordList();
+		int totalScore = 0;
+		for (String word : puzzle.currentWordList.keySet()) {
+			HashMap<String, Integer> wordInfo = puzzle.currentWordList.get(word);
+
+			int score = wordInfo.get("score");
+			int pangram = wordInfo.get("pangram");
+
+			totalScore += score;
+
+			String outputString = String.format("%s (%d)", word, score);
+			Color outputColor = (pangram == 1) ? Color.BLUE : Color.BLACK;
+
+			sbg.addWord(outputString, outputColor);
+			wordInfo.replace("found", 1);
+		}
+
+		String totalsOutput = String.format("%d words; %d points", puzzle.currentWordList.size(), totalScore);
+		sbg.showMessage(totalsOutput);
+	}
+
+	/**
+	 * Callback function for "Word" input field.
+	 * 
+	 * Checks the supplied value against the word list
+	 * for the current puzzle. Updates the game board with
+	 * scoring information if the word is found.
+	 * 
+	 * Will update the word list to reflect "found" status.
+	 * 
+	 * @param word The word to be checked
+	 */
+	private void guessAction(String word) {
+
+		try {
+
+			String validGuess = puzzle.isValidGuess(word);
+
+			// Get the word info from the current words list.
+			HashMap<String, Integer> wordInfo = puzzle.currentWordList.get(validGuess);
+
+			if (wordInfo.get("found") == 1) {
+				return;
+			}
+			;
+
+			int score = wordInfo.get("score");
+			int pangram = wordInfo.get("pangram");
+
+			String outputString = String.format("%s (%d)", validGuess, score);
+			Color outputColor = (pangram == 1) ? Color.BLUE : Color.BLACK;
+
+			sbg.addWord(outputString, outputColor);
+			wordInfo.replace("found", 1);
+
+			int currentScore = puzzle.getCurrentScore();
+			int currentWordCount = puzzle.getCurrentWordCount();
+			String totalsString = String.format("%d words; %d points", currentWordCount, currentScore);
+			sbg.showMessage(totalsString);
+
+			sbg.clearField("Word");
+
+		} catch (InvalidGuessException e) {
+			sbg.showMessage(e.getMessage(), Color.RED);
+		}
+	};
+
+	/**
+	 * Callback function to shuffle game board.
+	 * 
+	 * Displays a new string for the game board; keeps
+	 * the middle letter the same.
+	 * 
+	 */
+	private void shuffleAction() {
+
+		// Get current puzzle.
+		StringBuilder currentWord = new StringBuilder();
+		currentWord.append(sbg.getBeehiveLetters());
+
+		// Set aside middle letter.
+		char middleLetter = currentWord.charAt(0);
+		currentWord.deleteCharAt(0);
+
+		// Shuffle word.
+		List<String> currentWordAsList = Arrays.asList(currentWord.toString().split(""));
+		Collections.shuffle(currentWordAsList);
+
+		// Place middle letter back.
+		StringBuilder shuffledWord = new StringBuilder();
+		shuffledWord.append(middleLetter);
+
+		for (String letter : currentWordAsList) {
+			shuffledWord.append(letter);
+		}
+		;
+
+		// Display shuffled word.
+		sbg.setBeehiveLetters(shuffledWord.toString());
+	}
+
+
+	/**Functional but incomplete...
+	 * 
+	 * Wanted to generate an exhaustive list of puzzles that had between
+	 * 21 and 81 words as well as a pangram. Did the math, there will be
+	 * 26 choose 7 = 657,800 different combinations.
+	 * 
+	 * Wrote an inefficient nChoosek algorithm, after an hour of running
+	 * it handed out ~300 solutions. This randomizer works functionally, 
+	 * the rest of the list just needs to be generated.
+	 * 
+	 * @throws InvalidPuzzleException
+	 * @throws FileNotFoundException
+	 * 
+	 * 
+	 */
+	private void randomizeAction() {
+
+		
+		try {
+			Puzzle randomPuzzle = Puzzle.generateRandomPuzzle();
+			puzzleAction(randomPuzzle.letters);
+		} catch (FileNotFoundException fnf) {
+			
+		} catch (InvalidPuzzleException ip) {
+
+		}
+	}
+	
+    /* Constants */
+
+	private static final String ENGLISH_DICTIONARY = "EnglishWords.txt";
+
+	/* Private instance variables */
+
+	private SpellingBeeGraphics sbg;
+	private Puzzle puzzle;
+
+	/* Startup code */
+	public static void main(String[] args) {
+		new SpellingBee().run();
+	}
+
+}
+
+class InvalidPuzzleException extends Exception {
+	public InvalidPuzzleException(String errorMessage) {
+		super(errorMessage);
+	}
+}
+
+class InvalidGuessException extends Exception {
+	public InvalidGuessException(String errorMessage) {
+		super(errorMessage);
+	}
+}
